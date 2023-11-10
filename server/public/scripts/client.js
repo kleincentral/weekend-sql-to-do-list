f7fa0b115034cb7332293ef8531ac3e6c516c055
Mark Klein <kleincentral@gmail.com>
2023-11-10 14:32:51 -0600

    List items show up on the DOM, and server has a todoRoute

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,11 +4,42 @@ function getTodos() {
     axios({
         type: 'GET',
         url: '/todos'
+    }).then((res) => {
+        renderTodos(res.data)
+    }).catch((error)=>{
+        console.log("Error in GET", error)
+    })
+}
+
+function renderTodos(data) {
+    console.log("Todo's Rendering...")
+    const todos = document.getElementById('viewList');
+    console.log(todos)
+    todos.innerHTML = ''
+
+    for(let i = 0; i < data.length; i+= 1) {
+        let index = data[i];
+        todos.innerHTML += `
+        <li data-todoId=${index.id}
+        data-testid="toDoItem">
+            ${index.text}
+        </li>
+        `;
+    }
+}
+
+function addTask() {
+    console.log("Attempting to add task!")
+    let task = document.getElementById('todoText').value
+    axios({
+        type: 'POST',
+        url: '/todos',
+        data: {
+            todo: task
+        }
     }).then(res => {
         renderTodos(res.data)
     })
 }
 
-function renderTodos(todos) {
-    
-}
+getTodos()
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+router.get('/', (req,res) =>{
+    console.log("Get called!")
+    let text = `
+    SELECT * FROM "todos"
+    ORDER BY "id";`
+    pool.query(text)
+    .then((dbResult) => {
+      console.log(dbResult)
+      res.send(dbResult.rows)
+    }).catch((dbError)=>{
+      res.sendStatus(500)
+    })
+})
 
 
 module.exports = router;
